a4ecf64da9e8b46368d8730c6825b3e1826f2a8f
unknown <mabrukamustafam@gmail.com>
2023-07-10 17:58:07 +0300

    Answered

--- challenge.js
+++ challenge.js
@@ -1,2 +1,18 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+let num1 = 20;
+let num2 = 26;
+let num3 = 23;
+
+
+let largestNum;
+if (num1 >= num2 && num1 >= num3) {
+    largestNum = num1;
+} else if (num2 >= num1 && num2 >= num3) {
+    largestNum = num2;
+} else {
+    largestNum = num3;
+}
+
+
+console.log(`The largest number out of the three numbers is X ${largestNum}`);
